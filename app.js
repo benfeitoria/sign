ceb0e11a3c34bebeccb48b947422ca11dec8f48f
André Assis <andretefras@gmail.com>
2020-02-27 15:01:41 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        clipped: false,
+        name: 'John Doe',
+        role: 'Developer',
+        email: 'joh.doe',
+        phone: '+55 (99) 99999-9999',
+        url: {
+            blog: 'https://blog.benfeitoria.com',
+            site: 'https://benfeitoria.com',
+            instagram: null,
+            facebook: null,
+            linkedin: null,
+            twitter: null
+        }
+    },
+    methods: {
+
+    },
+    watch: {
+
+    },
+    computed: {
+        html() {
+            return ``;
+        }
+    },
+    created(){
+        const clipboard = new ClipboardJS('#copy-html');
+        clipboard.on('success', (e) => {
+            e.clearSelection();
+            this.clipped = true;
+        });
+    }
+});
