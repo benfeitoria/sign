46438030726a31397f44b0ae3ee17a5675f5db42
Jorge Junior <jorge.junior.568@live.com>
2020-03-02 17:55:47 -0300

    localStorage boosted and DO NOT use alert()

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     el: "#app",
     data: {
         clipped: false,
+        clippedTimeout: null,
         name: 'Ben Feitor',
         role: 'Colaborador',
         email: 'ben@benfeitoria.com',
@@ -16,13 +17,55 @@ const vm = new Vue({
             linkedin: "https://www.linkedin.com/company/benfeitoria/",
             twitter: "https://twitter.com/benfeitoria"
         },
-        phone_mask: '+## (##) ##### ####'
+        phone_mask: '+## (##) ##### ####',
+        storage_infos: ['name','role','email','phone']
     },
     methods: {
+        getStorageKeyName(info){
+            return `SignAppStorageData|${info}`
+        },
+        getStorageValue(info){
+            let data = JSON.parse(window.localStorage.getItem(
+                this.getStorageKeyName(info)
+            ));
 
+            return data !== null ? data : ''
+        },
+        setStorageValue(info,value){
+            window.localStorage.setItem(
+                this.getStorageKeyName(info),
+                JSON.stringify(value)
+            );
+        },
+        getInformationsFromStorage(){
+            for(let info of this.storage_infos){
+                if(this.getStorageValue(info).toString().length){
+                    this[info] = this.getStorageValue(info).toString();
+                }
+            }
+        },
+        clippedHandler(){
+            this.clipped = true;
+            clearTimeout(this.clearTimeout);
+
+            this.clearTimeout = setTimeout(() => {
+                this.clipped = false;
+            },8000);
+        }
     },
     watch: {
-
+        name(value){
+            this.setStorageValue("name",value);
+        },
+        role(value){
+            this.setStorageValue("role",value);
+        },
+        email(value){
+            this.setStorageValue("email",value);
+        },
+        phone(value){
+            this.setStorageValue("phone",value);
+        }
     },
     computed: {
         html() {
@@ -30,11 +73,14 @@ const vm = new Vue({
         },
     },
     created(){
+        this.getInformationsFromStorage();
+
+
         const clipboard = new ClipboardJS('#copy-html');
+
         clipboard.on('success', (e) => {
             e.clearSelection();
-            alert("Copiado!");
-            this.clipped = true;
+            this.clippedHandler();
         });
     }
 });
