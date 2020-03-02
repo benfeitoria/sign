e26ea4b143ecc1593c31cf2c7baf1f056446e66a
OKPhil <philipe.lagreca@gmail.com>
2020-03-02 12:19:41 -0300

    Fix copy on Firefox and Chrome

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const vm = new Vue({
         name: 'Ben Feitor',
         role: 'Colaborador',
         email: 'ben@benfeitoria.com',
-        phone: '+55 (99) 99999-9999',
+        phone: '+55 (99) 99999 9999',
         url: {
             blog: 'https://blog.benfeitoria.com',
             site: 'https://benfeitoria.com',
@@ -16,7 +16,7 @@ const vm = new Vue({
             linkedin: "https://www.linkedin.com/company/benfeitoria/",
             twitter: "https://twitter.com/benfeitoria"
         },
-        phone_mask: '+## (##) #### ####'
+        phone_mask: '+## (##) ##### ####'
     },
     methods: {
 
